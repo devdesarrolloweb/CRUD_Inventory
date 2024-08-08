ecad521ec09e607aab37644b3b178fa19b0ea6d2
dev.desarrolloweb <dev.desarrolloweb@gmail.com>
2024-08-08 17:44:05 -0600

    API

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // http://localhost:8000/api/v1/
 //https://www.myapi.wuaze.com/api/v1/
 
-export const API_URL = 'http://localhost:8000/api/v1/';
+export const API_URL = 'https://www.myapi.wuaze.com/api/v1/';
